ba947a9cf6bf788b4b7593640e7bc2354e68d545
AliAMoteirek <mtairek20@gmail.com>
2022-09-07 18:40:01 +0200

    Added a new IT

--- src/test/java/com/example/almspringvg/controller/AppIT.java
+++ src/test/java/com/example/almspringvg/controller/AppIT.java
@@ -2,7 +2,6 @@ package com.example.almspringvg.controller;
 
 import io.restassured.RestAssured;
 import io.restassured.parsing.Parser;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
